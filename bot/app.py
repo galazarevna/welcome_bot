25aea5c4c25a38b564f56cbc748c9f87590375d1
galazarevna <foringela@gmail.com>
2022-01-25 22:26:39 -0800

    Add parsing for resume request. Add typing emulation.

--- bot/app.py
+++ bot/app.py
@@ -1,4 +1,10 @@
+import re
+import time
+
+import telegram
 from telegram.ext import Updater, MessageHandler, Filters
+
+RESUME_PATTERN = r"^.*resume template.*\?.*|.*resume example.*\?.*|.*образец resume.*\?.*|.*образец резюме.*\?.*$"
 
 with open("token.txt", "r", encoding="utf8") as f:
     TOKEN = f.read()
@@ -8,7 +14,7 @@ disp = updater.dispatcher
 
 
 def new_member(update, context):
-    """ Identifies a new member in a grouup and sends a welcome message to the member.
+    """ Identifies a new member in a group and sends a welcome message to the member.
     """
     for member in update.message.new_chat_members:
         print(member)
@@ -18,12 +24,31 @@ def new_member(update, context):
             user = f"{member.first_name} {member.last_name}"
 
         text = f"Привет, {user}!\nПожалуйста, заполните опросник: \n1. Штат \n2. Текущая " \
-               f"должность \n3. Желаемая позиция \n4. Джава или Пайтон? "
+               f"должность \n3. Желаемая позиция \n4. Джава или Пайтон?"
         chat_id = update.message.chat.id
         context.bot.send_message(chat_id=chat_id, text=text)
 
 
+def typing(update, context, chat_id):
+    """Emulating real user's typing.
+    """
+    context.bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING, timeout=1)
+    time.sleep(0.5)
+
+
+def resume(update, context):
+    """ Parses for resume template queries and replies with a link to a resume example on a GDrive.
+    """
+    text = "Вот образец резюме (link)"
+    chat_id = update.message.chat.id
+    message_id = update.message.message_id
+    typing(update, context, chat_id)
+    context.bot.send_message(chat_id=chat_id, text=text, reply_to_message_id=message_id)
+
+
+# Add all handlers to the dispatcher and run the bot
 disp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
+disp.add_handler(MessageHandler(Filters.regex(re.compile(RESUME_PATTERN, re.IGNORECASE)), resume))
 
 updater.start_polling()
 updater.idle()
