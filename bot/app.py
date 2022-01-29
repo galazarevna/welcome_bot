f6cc20cca103889cd18d2e87e456295ddc894574
galazarevna <foringela@gmail.com>
2022-01-28 20:55:30 -0800

    Add java_python. Add portnov_testpro.

--- bot/app.py
+++ bot/app.py
@@ -1,3 +1,5 @@
+import os
+import random
 import re
 import time
 
@@ -5,6 +7,9 @@ import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 
 RESUME_PATTERN = r"^.*resume template.*\?.*|.*resume example.*\?.*|.*образец resume.*\?.*|.*образец резюме.*\?.*$"
+JAVA_PYTHON_PATTERN = r"^.*java or python.*\?.*|.*java или python.*\?.*|.*джав. или питон.*\?.*|.*джав. или пайтон.*\?.*$"
+PORTNOV_TESTPRO_PATTERN = r"^.*portnov или.* testpro.*\?.*|.*портнов. или.* testpro.*\?.*|.*портнов. или.* тестпро.*\?.*$"
+GIF = "https://tenor.com/view/jew-money-invest-gif-24178583"
 
 with open("token.txt", "r", encoding="utf8") as f:
     TOKEN = f.read()
@@ -46,9 +51,29 @@ def resume(update, context):
     context.bot.send_message(chat_id=chat_id, text=text, reply_to_message_id=message_id)
 
 
+def java_python(update, context):
+    """ Parses for java vs python holy war and replies with a random image from file.
+    """
+    path = "images/java_python/"
+    random_image = random.choice(os.listdir(path))
+    chat_id = update.message.chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(path + random_image, 'rb'))
+
+
+def portnov_testpro(update, context):
+    """ Parses for Portnov vs TestPro query and replies with a related gif.
+    """
+    chat_id = update.message.chat.id
+    context.bot.send_animation(chat_id=chat_id, animation=GIF)
+
+
 # Add all handlers to the dispatcher and run the bot
 disp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
 disp.add_handler(MessageHandler(Filters.regex(re.compile(RESUME_PATTERN, re.IGNORECASE)), resume))
+disp.add_handler(
+    MessageHandler(Filters.regex(re.compile(JAVA_PYTHON_PATTERN, re.IGNORECASE)), java_python))
+disp.add_handler(MessageHandler(Filters.regex(re.compile(PORTNOV_TESTPRO_PATTERN, re.IGNORECASE)),
+                                portnov_testpro))
 
 updater.start_polling()
 updater.idle()
